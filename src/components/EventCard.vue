86c10f691de96c884518f178e09f29f5b5a9bbc8
Sebastian Perry <sperry@biggby.com>
2019-11-07 14:26:12 -0500

    Initial commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <input v-model="quote" type="text" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
