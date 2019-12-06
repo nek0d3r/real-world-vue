4c595735e2030658cd8f4655753b1dba30bb52a8
Sebastian <darklink.perry@gmail.com>
2019-12-06 11:33:48 -0500

    Vuex state management

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Events Create</h1>
+  <div>
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped></style>
