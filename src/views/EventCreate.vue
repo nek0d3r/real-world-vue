fe68144478ae2da117085da7d576ef171cce0f25
Sebastian <darklink.perry@gmail.com>
2020-01-10 12:06:53 -0500

    Added notifications

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -68,7 +68,7 @@ export default {
   },
   methods: {
     createEvent() {
-      this.$store.dispatch('createEvent', this.event).then(() => {
+      this.$store.dispatch('event/createEvent', this.event).then(() => {
         this.$router.push({
           name: 'event-show',
           params: { id: this.event.id }
